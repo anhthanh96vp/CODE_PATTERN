b7cae060960074167a4b4fbd3326f247cabc0138
Thanh Na <anhthanh1996vp@gmail.com>
2018-08-01 02:18:35 +0700

    up connect postgresql

---
+++ nodejs/postgresql-connect/routers/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+import { sequelize, Op } from "../sequelize";
+
+router.use("/users", require(__dirname + "/users"))
+
+//Test DB connection
+sequelize
+	.authenticate()
+	.then(() => {
+		console.log("Connection has been established successfully.");
+	})
+	.catch(err => {
+		console.error("Unable to connect to the database:", err);
+	});
+
+
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
